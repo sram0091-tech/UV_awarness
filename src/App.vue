26857f0c4c3248cc7382a5c1b47447cbc1eb6441
Surya Prakash <suryaprakash020010@gmail.com>
2026-03-15 22:53:17 +1100

    Added team photo and added description and removed right top floating text

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
-import TitleBlock from './components/TitleBlock.vue'
 </script>
 
 <template>
   <Navbar />
-  <TitleBlock />
   <router-view />
 </template>
